a6d067c0f02d7f32fd3dc05fd9b330f7ca0f15a9
Tomer Ben David <tomerno6@gmail.com>
2026-04-30 14:13:24 +0800

    fix CORS allow header

--- cloudflare/reactions-worker.js
+++ cloudflare/reactions-worker.js
@@ -14,7 +14,7 @@ export default {
     const cors = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
 
     if (request.method === "OPTIONS") {
